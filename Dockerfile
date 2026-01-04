f7c6999d903bc2feb93a517a222e445977a91f6d
kovardin <artem-kovardin@yandex.ru>
2026-01-04 15:36:11 +0300

    new images

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,17 @@ FROM rust:1.92-alpine AS builder
 
 # Устанавливаем зависимости для сборки
 RUN apk add --no-cache musl-dev openssl-dev pkgconfig
+# RUN apk add --no-cache x86_64-linux-musl-gcc
+
+# Устанавливаем целевой архитектуру для Linux
+RUN rustup target add x86_64-unknown-linux-musl
 
 WORKDIR /usr/src/app
 
 # Копируем  весь исходный код
 COPY . .
 RUN cargo fetch --locked
-RUN cargo build --release --frozen
+RUN cargo build --release --target x86_64-unknown-linux-musl --frozen
 
 # Этап рантайма
 FROM alpine:latest
@@ -20,7 +24,7 @@ RUN apk add --no-cache libgcc openssl
 WORKDIR /app
 
 # Копируем бинарник и ассеты
-COPY --from=builder /usr/src/app/target/release/anonimus-animals /app/
+COPY --from=builder /usr/src/app/target/x86_64-unknown-linux-musl/release/anonimus-animals /app/
 COPY --from=builder /usr/src/app/assets /app/assets
 
 EXPOSE 8000
--- src/handlers.rs
+++ src/handlers.rs
@@ -63,7 +63,7 @@ pub async fn update(State(state): State<AppState>, Json(json): Json<UpdateReques
 pub async fn animals(State(state): State<AppState>) -> impl IntoResponse {
     let duration = Utc::now() -  Duration::minutes(1);
 
-    let query = "SELECT id, name, image, created_at, updated_at FROM animals WHERE updated_at > $1";
+    let query = "SELECT id, name, image, created_at, updated_at FROM animals WHERE updated_at > $1 LIMIT 5";
     let res = sqlx::query_as::<_, Animal>(query)
         .bind(duration)
         .fetch_all(&state.db)
@@ -84,7 +84,7 @@ pub async fn animals(State(state): State<AppState>) -> impl IntoResponse {
 pub async fn add(State(state): State<AppState>) -> impl IntoResponse {
     let name = state.generator.generate();
     let parts: Vec<&str> = name.split_whitespace().collect();
-    let image = format!("{}.png", parts[1]);
+    let image = format!("{}_.png", parts[1]);
 
     let res = sqlx::query("INSERT INTO animals (name, image, created_at, updated_at) VALUES ($1, $2, $3, $4)")
         .bind(name)
